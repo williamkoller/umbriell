aa6b3c6c80b69bc077605c09a8e66e08a363a906
William Koller <williamkoller30@gmail.com>
2023-08-23 00:34:39 -0300

    feat: add pagination in usecases

--- src/data/usecases/user/load-users/db-load-users.ts
+++ src/data/usecases/user/load-users/db-load-users.ts
@@ -1,13 +1,20 @@
 import { LoadUsersRepository } from '@app/data/protocols/db/user/load-users-repository';
+import { FilterUserModel } from '@app/domain/models/user/filter-user.model';
+import { PageOptionsModel } from '@app/domain/models/user/page-options.model';
 import { UserModel } from '@app/domain/models/user/user.model';
 import { LoadUsers } from '@app/domain/usecases/user/load-users/load-users';
-import { UserPresenter } from '@app/presentation/presenter/user/user.presenter';
+import { PaginationResponseDto } from '@app/presentation/dtos/pagination/pagination.dto';
 
 export class DbLoadUsers implements LoadUsers {
   constructor(private readonly loadUsersRepository: LoadUsersRepository) {}
 
-  async load(): Promise<UserModel[]> {
-    const users = await this.loadUsersRepository.load();
-    return new UserPresenter().formatArray(users);
+  async load(
+    filterUserModel: FilterUserModel,
+    pageOptionsModel: PageOptionsModel,
+  ): Promise<PaginationResponseDto<UserModel>> {
+    return await this.loadUsersRepository.load(
+      filterUserModel,
+      pageOptionsModel,
+    );
   }
 }
--- src/domain/usecases/user/load-users/load-users.ts
+++ src/domain/usecases/user/load-users/load-users.ts
@@ -1,5 +1,11 @@
+import { FilterUserModel } from '@app/domain/models/user/filter-user.model';
+import { PageOptionsModel } from '@app/domain/models/user/page-options.model';
 import { UserModel } from '@app/domain/models/user/user.model';
+import { PaginationResponseDto } from '@app/presentation/dtos/pagination/pagination.dto';
 
 export interface LoadUsers {
-  load: () => Promise<UserModel[]>;
+  load: (
+    filterUserModel: FilterUserModel,
+    PageOptionsModel: PageOptionsModel,
+  ) => Promise<PaginationResponseDto<UserModel>>;
 }
