a1694ba1d341c1bf2f008fca7e59b6e47f12d2aa
William Koller <williamkoller30@gmail.com>
2023-09-25 14:01:54 -0300

    feat: set swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from '@app/infra/ioc/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import * as Sentry from '@sentry/node';
+import { AllExceptionsFilter } from './main/common/filters/all-exception.filter';
+import { SentryConfig } from './main/config/sentry/sentry-config';
+import { swaggerConfig } from './main/config/docs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -11,17 +15,25 @@ async function bootstrap() {
     autoFlushLogs: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  swaggerConfig(app);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new AllExceptionsFilter());
   app.setGlobalPrefix('api/v1');
 
   const config = app.get<ConfigService>(ConfigService);
   const port = config.get<number>('PORT');
   const appName = config.get<string>('APP_NAME');
 
+  new SentryConfig(new ConfigService()).init(app);
+
+  app.use(Sentry.Handlers.requestHandler());
+  app.use(Sentry.Handlers.errorHandler());
+
   const logger = new Logger(`Main - ${appName}`);
 
-  await app.listen(port, async () =>
-    logger.log(`Server running at ${await app.getUrl()}`),
-  );
+  await app.listen(port);
+  const url = await app.getUrl();
+  logger.log(`Server running at: ${url}/api/docs`);
 }
 bootstrap();
